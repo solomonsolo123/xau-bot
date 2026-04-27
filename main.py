8af07f0bd958b52f26ab74f3be0796f8ad11b40a
solomonsolo123 <solomon2008.p@gmail.com>
2026-04-27 07:49:26 -0700

    v1.2
    telegram bot added

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Scanning markets...")
+import os
+import requests
+
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+msg="Bot is running from GitHub!"
+
+url=f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+requests.post(
+    url,
+    data={
+      "chat_id": CHAT_ID,
+      "text": msg
+    }
+)
